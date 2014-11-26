7e075fcb593559ada7a3f684a6a7a986c7394e73
jeroniInf <jeroni.carandell@infantium.com>
2014-11-26 17:38:19 +0100

    Add utils and weekday finder

---
+++ utils.py
@@ -0,0 +1,27 @@
+__author__ = 'jeronicarandellsaladich'
+
+import datetime
+
+
+def date_to_day_of_week(date):
+    '''
+    takes a date and returns day of the week 
+    :return:
+    '''
+    print date
+    date = datetime.datetime.strptime(date, "%d/%M/%y")
+    return date.weekday()
+
+
+def int_to_weekday(day):
+    '''
+    Returns day of the week from an integer
+    :param day:
+    :return:
+    '''
+    week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    return week[day]
+
+# TEST
+# date = "3/2/08"
+# print int_to_weekday(date_to_day_of_week(date))
