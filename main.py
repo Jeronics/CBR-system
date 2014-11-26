5cb272167a0e5c613966b5c148b8ac4a6b051bd6
jeroniInf <jeroni.carandell@infantium.com>
2014-11-26 16:18:09 +0100

    Create main for python CBR project

---
+++ main.py
@@ -0,0 +1,14 @@
+__author__ = 'denis_iosu_jeroni_pablo'
+
+def main():
+    # TODO:
+    #   0. FIND AND LOAD DATA
+    #       a. Here we have history of 'quinielas': http://www.resultados-futbol.com/quiniela/historico/18
+    #   1. FILTER DATA
+    #   2. RETRIEVE
+    #   3. REUSE
+    #   4. REVISE
+    #   5. RETAIN
+
+if __name__ == '__main__':
+    main()
