5a3d4506443e99df464eef281e570712f854c806
Pablo Pardo Garcia <ppardoga7@gmail.com>
2014-11-26 16:49:10 +0100

    Add sources in the comments

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ def main():
     # TODO:
     #   0. FIND AND LOAD DATA
     #       a. Here we have history of 'quinielas': http://www.resultados-futbol.com/quiniela/historico/18
+    #       b. Here we have the statistics of the 2014-15: http://www.lfp.es/estadisticas/liga-bbva/goles/
+    #       c. Here we can get everything!!: http://www.marca.com/estadisticas/futbol/primera/2010_11/
     #   1. FILTER DATA
     #   2. RETRIEVE
     #   3. REUSE
