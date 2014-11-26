92e12d1223e846903912af149564cdfe88f4b2c4
imendibo <iosu.mendizabal@gmail.com>
2014-11-26 17:48:22 +0100

    gitignore

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ def main():
     #       a. Here we have history of 'quinielas': http://www.resultados-futbol.com/quiniela/historico/18
     #       b. Here we have the statistics of the 2014-15: http://www.lfp.es/estadisticas/liga-bbva/goles/
     #       c. Here we can get everything!!: http://www.marca.com/estadisticas/futbol/primera/2010_11/
+    #
+    #
+    # 
     #   1. FILTER DATA
     #   2. RETRIEVE
     #   3. REUSE
