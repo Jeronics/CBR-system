7ab983f655f8c63453a3b691577f8882c4e9728f
imendibo <iosu.mendizabal@gmail.com>
2014-11-26 17:09:35 +0100

    good info

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ __author__ = 'denis_iosu_jeroni_pablo'
 def main():
     # TODO:
     #   0. FIND AND LOAD DATA
+    #       -a. We have csv-s of first and second division of spain from 1996/97 til now. And lot of information else.
+    #
+    #
+    #                                http://www.football-data.co.uk/spainm.php
+    #
+    #
     #       a. Here we have history of 'quinielas': http://www.resultados-futbol.com/quiniela/historico/18
     #       b. Here we have the statistics of the 2014-15: http://www.lfp.es/estadisticas/liga-bbva/goles/
     #       c. Here we can get everything!!: http://www.marca.com/estadisticas/futbol/primera/2010_11/
